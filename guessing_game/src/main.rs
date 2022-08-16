57abdcc3be104a63ac5fd11e4dc1dd50daf44205
Manuel <manuelinfosec@gmail.com>
2022-08-16 12:40:46 +0100

    Guessing Game: Can now collect user's input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input the guess: ");
+
+    let mut guess = String::new();
+
+    std::io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line!");
+
+    println!("You guessed : {}", guess);
 }
