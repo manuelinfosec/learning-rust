811cfa5553c917365a7ee791e1072c878018ffc7
Manuel <manuelinfosec@gmail.com>
2022-09-09 03:22:19 +0100

    Generating random numbers with `rand::Rng` trait

---
+++ README.md
@@ -0,0 +1,2 @@
+# learning-rust
+source files personally used for learning to build low-level computer programs with Rust 🦀.
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,16 @@
+// rand was installed externally
+extern crate rand;
+
+// import Rng trait from rand
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
+    let secret_numer = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is {}", secret_numer);
+
+    // display for prompt
     println!("Please input the guess: ");
 
     let mut guess = String::new();
@@ -7,6 +18,14 @@ fn main() {
     std::io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line!");
-
-    println!("You guessed : {}", guess);
+    //parse string to specified data type
+    let guess: u32 = guess.trim()
+                        .parse::<u32>()
+    // compare guess with secret number and return Enums from Ordering.
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+>>>>>>> 99182f9 (Comparison with `Ordering` & Parsing string to unsigned integer)
 }
